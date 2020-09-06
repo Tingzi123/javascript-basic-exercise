7d1205464663a566866d19f857482bc9e8100a60
Tingzi123 <1044373556@qq.com>
2020-09-06 15:48:37 +0800

    [chenting] fix: flattens a nested array into a sequence

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,29 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
 
-  throw new Error('Please delete this line and implement the function');
+  if (array.length === 0) {
+    return [];
+  }
+
+  function isArr(arr, r) {
+    if (Object.prototype.toString.call(arr) === '[object Array]') {
+      arr.forEach((i) => {
+        r.push(i);
+      });
+    } else {
+      r.push(arr);
+    }
+    return r;
+  }
+
+  let re = [];
+  for (let i = 0; i < array.length; i += 1) {
+    re = isArr(array[i], re);
+  }
+
+  return re;
 }
