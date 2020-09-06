c20af4552b6f1fbea2476c40a97a01df9bf2bf95
Tingzi123 <1044373556@qq.com>
2020-09-06 14:52:22 +0800

    [chenting] fix: creates a linked list from an array

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,23 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array === undefined) {
+    throw new Error('Creating list from undefined array');
+  }
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  const header = { value: array[0], next: null };
+  let obj = header;
+  for (let i = 1; i < array.length; i += 1) {
+    obj.next = { value: array[i], next: null };
+    obj = obj.next;
+  }
+  return header;
 }
